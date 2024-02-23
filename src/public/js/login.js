8db91dc0a21143e372127a61016dd255837bf3b1
Yoel Betancor <juyobema@gmail.com>
2024-02-23 14:46:56 +0000

    fixed json

--- src/public/js/login.js
+++ src/public/js/login.js
@@ -3,7 +3,7 @@ console.log("hola")
 
  const sendLogin= async ()=>{
     console.log("hola estoy en login")
-    const response = await fetch("/hola", {method:'POST',  headers: {'Accept': 'application/json','Content-Type': 'application/json'}, body:JSON.stringify("hola")})
+    const response = await fetch("/hola", {method:'POST',  headers: {'Accept': 'application/json','Content-Type': 'application/json'}, body:JSON.stringify({key:"hola"})})
     const result= await response.json()
     console.log(result)
  }
